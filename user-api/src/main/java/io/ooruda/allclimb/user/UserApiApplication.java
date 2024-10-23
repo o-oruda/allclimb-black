2a85be6538ff5f0b4b3f3f115aaec84836597302
hyun-sun <itosamto.hshsy@gmail.com>
2024-10-23 22:33:55 +0900

    fix: build error

--- user-api/src/main/java/io/ooruda/allclimb/user/UserApiApplication.java
+++ user-api/src/main/java/io/ooruda/allclimb/user/UserApiApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaAuditing
-@EnableJpaRepositories(basePackages = {"io.ooruda.allclimb.module.database"})
-@EntityScan(basePackages = {"io.ooruda.allclimb.module.database"})
+@EnableJpaRepositories(basePackages = {"io.ooruda.allclimb.module.database", "io.ooruda.allclimb.user"})
+@EntityScan(basePackages = {"io.ooruda.allclimb.module.database", "io.ooruda.allclimb.user"})
 @SpringBootApplication(scanBasePackages = {"io.ooruda.allclimb"})
 public class UserApiApplication {
 
